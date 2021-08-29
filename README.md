20c9152df4b8c608fd45f88cc7fa07f3f0adba13
GoodGuyAsh <67311318+GoodGuyAsh@users.noreply.github.com>
2021-08-29 12:49:32 +0530

    minor changes in readme

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Machine Design
 
 ### Design of Knuckle Joint:
--  
+
+- Axial load,
+- Factor of Safety (not given will be assumed to be 5)
+- Yield Strength of the Material (Compressive yield and Tensile Yield will be assumed equal)
---
+++ designer.py
@@ -0,0 +1,72 @@
+"""
+The program can be used to design knuckle joints
+[
+    PARAMETERS = Axial load,
+                 Factor of Safety (not given will be assumed to be 5)
+                 Yield Strength of the Material  (Compressive yield and Tensile Yield will be assumed equal)
+]
+"""
+import math
+
+def roundup(x, y):
+    return int(math.ceil(x/y))*y
+
+def standard(x):
+    if x<60 and x>25: return roundup(x,5)
+
+
+class Knuckle:
+    def __init__(self, load, yield_strength, fos=None, tensile_stress=None, crushing_stress=None, shear_stress=None):
+        self.load = load
+        self.fos = 5 if fos==None else fos
+        self.yield_strength = yield_strength
+
+        # Permissible Stresses
+        self.tensile_stress = (self.yield_strength/self.fos) if tensile_stress==None else tensile_stress
+        self.crushing_stress = (self.yield_strength/self.fos) if crushing_stress==None else crushing_stress 
+        self.shear_stress = (self.yield_strength/self.fos)/2 if shear_stress==None else shear_stress
+        
+
+    def dimensions(self):
+        self.d = standard((math.sqrt((4*self.load)/(math.pi*self.tensile_stress))))
+
+        self.d1 = self.d
+        self.d2 = 2*self.d
+        self.d3 = 1.5*self.d
+        self.t = 1.25*self.d
+        self.t1 = 0.75*self.d
+        self.t2 = 0.5*self.d
+        return f"{self.d, self.d1, self.d2, self.d3,self.t,self.t1, self.t2}"
+
+    def pin_failure_in_shear(self):
+        self.d1_ = math.sqrt((2*self.load)/(math.pi*self.shear_stress))
+        return True if self.d1_<=self.d1 else False
+
+    def rod_end_failure_in_tension(self):
+        self.sigma_t1 = self.load/((self.d2 - self.d1)*self.t)
+        return True if self.sigma_t1 <= self.tensile_stress else False
+        
+    def rod_end_failure_in_shear(self):
+        self.tau1 = self.load/((self.d2 - self.d1)*self.t)
+        return True if self.tau1 <= self.shear_stress else False
+
+    def rod_end_failure_in_crushing(self):
+        self.sigma_c1 = self.load/(self.d1*self.t)
+        return True if self.sigma_c1 <= self.crushing_stress else False
+     
+    def forked_end_failure_in_tension(self):
+        self.sigma_t2 = self.load/(2*(self.d2 - self.d1)*self.t1)
+        return True if self.sigma_t2 <= self.tensile_stress else False
+
+    def forked_end_failure_in_shear(self):
+        self.tau2 = self.load/(2*(self.d2 - self.d1)*self.t1)
+        return True if self.tau2 <= self.shear_stress else False
+
+    def forked_end_failure_in_crushing(self):
+        self.sigma_c2 = self.load/(2*self.d1*self.t1)
+        return True if self.sigma_c2 <= self.tensile_stress else False
+
+if __name__ == '__main__':
+    joint = Knuckle(80000, 400)
+    print(joint.dimensions())
+    
